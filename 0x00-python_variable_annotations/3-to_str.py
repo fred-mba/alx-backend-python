7aabdee09c5ddc2915ae2649a0372dad77c6d6b5
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-07-06 05:55:31 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A module that takes a value of n and returns the string rep of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns str representantion of the float.
+
+    Parameters
+    ----------
+    n(float): The floating point number.
+
+    Returns
+    -------
+    str: The string value of n.
+    """
+    return repr(n)
