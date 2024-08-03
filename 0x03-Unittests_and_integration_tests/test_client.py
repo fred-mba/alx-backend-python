e2e66b0d635981fd66a1c33cc2751c6fc6719b14
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-03 22:01:52 +0300

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -48,3 +48,62 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(
                 GithubOrgClient("google")._public_repos_url,
                 "https://api.github.com/users/google/repos")
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """
+        Unit-test GithubOrgClient.public_repos
+        """
+        test_payload = {
+            'repos': [
+                {
+                    "id": 1936771,
+                    "name": "truth",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/truth",
+                    "created_at": "2011-06-22T18:55:12Z",
+                    "updated_at": "2024-07-29T19:13:42",
+                    "language": "Java",
+                    "has_issues": True,
+                    "watchers": 2712,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 3975462,
+                    "name": "anvil-build",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/anvil-build",
+                    "created_at": "2012-04-09T20:09:24Z",
+                    "updated_at": "2023-12-22T11:30:23Z",
+                    "language": "Python",
+                    "has_issues": True,
+                    "watchers": 56,
+                    "default_branch": "master",
+                },
+            ]
+        }
+
+        mock_get_json.return_value = test_payload["repos"]
+
+        public_repos_url = "https://api.github.com/orgs/google/repos"
+        with patch(
+                "client.GithubOrgClient._public_repos_url",
+                new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "public_repos_url"
+
+            self.assertEqual(
+               GithubOrgClient("google").public_repos(),
+               ["truth", "anvil-build"])
+
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
